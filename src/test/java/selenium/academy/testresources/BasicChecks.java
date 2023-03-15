4811786f7b3721b5d8e911f817b076b1cb167404
Mahesh <mahesh.thammanavenie@gmail.com>
2023-03-15 23:51:21 +0530

    ExcelUtility and Data Provider

--- src/test/java/selenium/academy/testresources/BasicChecks.java
+++ src/test/java/selenium/academy/testresources/BasicChecks.java
@@ -1,15 +1,21 @@
 package selenium.academy.testresources;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import selenium.academy.pageobjects.LandingPage;
 import selenium.academy.tests.BaseTest;
+import selenium.academy.tests.ExcelUtility;
 
 public class BasicChecks extends BaseTest {
 	
@@ -21,8 +27,14 @@ public class BasicChecks extends BaseTest {
 		driver=initializeBrowser();
 		
 	}
-	@Test
-	public void login() throws IOException {
+	@DataProvider(name="getLoginDetails")
+	public Object[][] loginDetails(){
+		return new Object[][] {{"t.maheshbsc@gmail.com","Qwe213as@"}, {"test@test.com","Qwe213as@"}};
+		
+	}
+	
+	@Test(dataProvider="getLoginDetails")
+	public void login(String UserName,String Password) throws IOException {
 		LandingPage lp=new LandingPage(driver);	
 		lp.loginToApp("t.maheshbsc@test.com", "Qwe213as@");
 	}
@@ -37,6 +49,16 @@ public class BasicChecks extends BaseTest {
 	public void verifyForgotPassword() {
 		LandingPage lp=new LandingPage(driver);	
 		lp.forgotPassword("best@test.com", "test1234");
+	}
+	@Test
+	public void getExcelDataTest() {
+		List<Map<String, String>> dataList = new ArrayList<>();
+		String ExcelFile="C:\\Users\\tmahe\\eclipse-workspace\\academy\\src\\main\\java\\selenium\\academy\\testdata//LoginData.xlsx";
+		ExcelUtility excel=new ExcelUtility();
+		dataList=excel.getExcelDataAsListofMaps(ExcelFile, "Login");
+		System.out.println(dataList);
+		
+		
 	}
 	
 	@AfterMethod
--- src/test/java/selenium/academy/tests/ExcelUtility.java
+++ src/test/java/selenium/academy/tests/ExcelUtility.java
@@ -7,30 +7,56 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtility {
 
-	public static List<LinkedHashMap<String, String>> getExcelDataAsMap(String excelFileName, String sheetName) throws IOException {
+	public static List<Map<String, String>> getExcelDataAsListofMaps(String excelFileName, String sheetName) {
 		// Create a Workbook
 		
-		List<LinkedHashMap<String, String>> dataList = new ArrayList<>();
-		FileInputStream fis=new FileInputStream("C:\\Users\\tmahe\\eclipse-workspace\\academy\\src\\main\\java\\selenium\\academy\\testdata//LoginData.xlsx");
-		XSSFWorkbook wb=new XSSFWorkbook(fis);
-		XSSFSheet sh=wb.getSheet(sheetName);
-		LinkedHashMap<Integer, String> map = new LinkedHashMap<Integer, String>();
-
-		for (int r = 0; r <= sh.getLastRowNum(); r++) {
-			int key = (int)sh.getRow(r)
-                      .getCell(0)
-                      .getNumericCellValue();
-			String value = sh.getRow(r)
-                           .getCell(1)
-                           .getStringCellValue();
-			map.put(key, value);
-    }
+		List<Map<String, String>> dataList = new ArrayList<>();
+		List<String> headers=new ArrayList<>();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(excelFileName);
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet sh=wb.getSheet(sheetName);
+			XSSFRow firstRow=sh.getRow(0);
+			int length=firstRow.getLastCellNum();
+			for(int i=0;i<length;i++) {
+				XSSFCell cell=firstRow.getCell(i);
+				headers.add(cell.toString());
+			}
+			System.out.println("headers" +headers);
+			int lastRowNum=sh.getLastRowNum();
+			for(int j=1;j<=lastRowNum;j++) {
+				Map<String,String> rowData=new HashMap<>();
+				XSSFRow row=sh.getRow(j);
+				String cellValue="";
+				for(int k=0;k<length;k++) {
+					XSSFCell c=row.getCell(k);
+					if(c==null || c.getCellType()==CellType.BLANK) {
+						cellValue="";
+					}else {
+						cellValue=row.getCell(k).toString();
+					}
+					rowData.put(headers.get(k), cellValue);
+							
+				}
+				dataList.add(rowData);
+			}
+			
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("IN progress");
 		return dataList;
 		
